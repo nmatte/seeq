84a9b0e661ff8b4a940a925b719670342c24070f
Nathan Matteson <nwmatteson@gmail.com>
2018-02-23 13:49:07 -0800

    Add loop service to providers.

--- src/app/audio/audio.module.ts
+++ src/app/audio/audio.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PlayBeat } from './play-beat.middleware';
+import { AudioContextService } from './audio-context.service';
+import { MatrixLoopService } from './matrix-loop.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: [PlayBeat]
+  providers: [AudioContextService, MatrixLoopService]
 })
 export class AudioModule { }
