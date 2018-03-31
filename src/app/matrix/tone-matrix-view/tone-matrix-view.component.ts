0928565d3b03bc6b6959f542002ebd14a365bf4a
Nathan Matteson <nwmatteson@gmail.com>
2018-03-31 10:43:44 -0700

    Clean up some async data

--- src/app/matrix/tone-matrix-view/tone-matrix-view.component.ts
+++ src/app/matrix/tone-matrix-view/tone-matrix-view.component.ts
@@ -23,9 +23,7 @@ export class ToneMatrixViewComponent implements OnInit {
   @select(['toneMatrix', 'time']) readonly lastBeatTime$: Observable<number>;
   @select(['toneMatrix', 'availableNotes']) readonly availableNotes$: Observable<string[]>;
   @select(['toneMatrix', 'beat']) readonly activeBeat$: Observable<number>;
-  activeBeat: number;
   availableNotes: string[];
-  lastRecordedBeat: number;
 
   constructor(private audioContextService: AudioContextService, 
               private ngRedux: NgRedux<IAppState>,
@@ -37,13 +35,6 @@ export class ToneMatrixViewComponent implements OnInit {
       ['A4', 'G4', 'E4', 'D4', 'C4', 'A3', 'G3', 'E3', 'D3', 'C3', 'A2'], 
       8
     ))
-
-    this.availableNotes$.subscribe(available => {
-      this.availableNotes = available
-    });
-
-    this.activeBeat$.subscribe(beat => this.activeBeat = beat)
-    this.lastBeatTime$.subscribe(time => this.lastRecordedBeat = time);
   }
 
   matrixToggle(note: string, index: number): void {
